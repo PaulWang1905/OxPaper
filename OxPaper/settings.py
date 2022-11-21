0d52e776c612162dfd31c62ae1c535bb4a46db70
Puyu Wang <puyuwang@outlook.com>
2022-11-21 22:15:56 +0000

    Update settings.py

--- OxPaper/settings.py
+++ OxPaper/settings.py
@@ -27,7 +27,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-!3)aws#vw30r%bge)!n1n5l^khy)ezql$aqve9(^_h6kb!24p_'
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
